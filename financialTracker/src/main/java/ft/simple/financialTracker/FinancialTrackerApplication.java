6a1b0c802aac5a019d59d344d4bf664c032d4589
Casey-Lux <casey.lux.000@gmail.com>
2026-05-03 17:48:41 -0500

    Adding initial spring application

---
+++ financialTracker/src/main/java/ft/simple/financialTracker/FinancialTrackerApplication.java
@@ -0,0 +1,13 @@
+package ft.simple.financialTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTrackerApplication.class, args);
+	}
+
+}
---
+++ financialTracker/src/test/java/ft/simple/financialTracker/FinancialTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ft.simple.financialTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
