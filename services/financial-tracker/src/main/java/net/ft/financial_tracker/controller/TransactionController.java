b98cca0a243c4054a3d489d293a71a685b979870
Casey-Lux <casey.lux.000@gmail.com>
2026-06-26 18:20:19 -0500

    Adding basic endpoints

--- services/financial-tracker/src/main/java/net/ft/financial_tracker/controller/TransactionController.java
+++ services/financial-tracker/src/main/java/net/ft/financial_tracker/controller/TransactionController.java
@@ -3,14 +3,21 @@ package net.ft.financial_tracker.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import net.ft.financial_tracker.service.TransactionJpaService;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
 public class transactionController{
+    final TransactionJpaService service;
 
-    @GetMapping("/history")
-    public String history(){
-        return "this is the history";
+    @GetMapping()
+    public List<Transaction> history(){
+        return service.findAll();
+    }
+
+    @PostMapping()
+    public Transaction save(Transaction transaction){
+        return service.save(transaction);
     }
 
 }
