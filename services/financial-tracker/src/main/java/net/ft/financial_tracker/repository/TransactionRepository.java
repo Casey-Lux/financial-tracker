bfabc859314a7af9a215966151976235c87a7a83
Casey-Lux <casey.lux.000@gmail.com>
2026-06-26 18:18:25 -0500

    Adding repository for transactions

---
+++ services/financial-tracker/src/main/java/net/ft/financial_tracker/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+package net.ft.financial_tracker.repository;
+
+import net.ft.financial_tracker.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+    List<Transaction> findById(Long id);
+
+    Optional<Transaction> findByDescription(String description);
+}
