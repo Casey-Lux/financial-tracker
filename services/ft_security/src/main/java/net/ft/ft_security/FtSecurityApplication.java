083048b2279d9cbe5c43f50dd52d2f255a619623
Casey-Lux <casey.lux.000@gmail.com>
2026-07-03 11:45:49 -0500

    Adding security service project

---
+++ services/ft_security/src/main/java/net/ft/ft_security/FtSecurityApplication.java
@@ -0,0 +1,13 @@
+package net.ft.ft_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtSecurityApplication.class, args);
+	}
+
+}
---
+++ services/ft_security/src/test/java/net/ft/ft_security/FtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ft.ft_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
