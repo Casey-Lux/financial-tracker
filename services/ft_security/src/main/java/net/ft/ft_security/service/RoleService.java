c12973abc65bc75197e49bf0f652a90e70f94500
Casey-Lux <casey.lux.000@gmail.com>
2026-07-09 13:43:58 -0500

    Fix: Typing errors

--- services/ft_security/src/main/java/net/ft/ft_security/service/RoleService.java
+++ services/ft_security/src/main/java/net/ft/ft_security/service/RoleService.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
 import net.ft.ft_security.model.Role;
 import net.ft.ft_security.repository.RoleRepository;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class RoleService{
     private final RoleRepository repository;
 
-    public Optional<Role> save(Role role){
+    public Role save(Role role){
         return repository.save(role);
     }
 }
