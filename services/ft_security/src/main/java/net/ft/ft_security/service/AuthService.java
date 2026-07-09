20a950e024241a15f731541647bad9ea39fdeb93
Casey-Lux <casey.lux.000@gmail.com>
2026-07-09 16:26:19 -0500

    Adding password encoder to register method on auth service

--- services/ft_security/src/main/java/net/ft/ft_security/service/AuthService.java
+++ services/ft_security/src/main/java/net/ft/ft_security/service/AuthService.java
@@ -1,6 +1,7 @@
 package net.ft.ft_security.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import lombok.AllArgsConstructor;
 import net.ft.ft_security.model.User;
 import net.ft.ft_security.model.Role;
@@ -14,14 +15,15 @@ import java.util.List;
 public class AuthService{
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User register(String email, String username, String password){
-        Role role = roleRepository.findById(1L).
+        Role role = roleRepository.findById(3L).
             orElseThrow(() -> new RuntimeException("Rol no encontrado"));
         User user = new User();
         user.setEmail(email);
         user.setUsername(username);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
         user.setRoleList(List.of(role));
         return userRepository.save(user);
     }
