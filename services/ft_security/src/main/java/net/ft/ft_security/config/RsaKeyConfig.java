d05b9d599742a403b770f8e86cc7864f3dd6ad80
Casey-Lux <casey.lux.000@gmail.com>
2026-07-15 09:36:52 -0500

    Adding RSA key configuration file

---
+++ services/ft_security/src/main/java/net/ft/ft_security/config/RsaKeyConfig.java
@@ -0,0 +1,55 @@
+package net.ft.ft_security.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Configuration
+public class RsaKeyConfig {
+
+    @Value("${spring.application.security.jwt.private-key}")
+    private Resource privateKeyResource;
+
+    @Value("${spring.application.security.jwt.public-key}")
+    private Resource publicKeyResource;
+
+    @Bean
+    public KeyPair jwtKeyPair() throws Exception {
+        RSAPrivateKey privateKey = loadPrivateKey();
+        RSAPublicKey publicKey = loadPublicKey();
+        return new KeyPair(publicKey, privateKey);
+    }
+
+    private RSAPrivateKey loadPrivateKey() throws Exception {
+        String content = new String(privateKeyResource.getInputStream().readAllBytes())
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] keyBytes = Base64.getDecoder().decode(content);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return (RSAPrivateKey) kf.generatePrivate(spec);
+    }
+
+    private RSAPublicKey loadPublicKey() throws Exception {
+        String content = new String(publicKeyResource.getInputStream().readAllBytes())
+                .replace("-----BEGIN PUBLIC KEY-----", "")
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] keyBytes = Base64.getDecoder().decode(content);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return (RSAPublicKey) kf.generatePublic(spec);
+    }
+}
