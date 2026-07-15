e740e0610123d009a58d07e9b71c58fb29ee4f9e
Casey-Lux <casey.lux.000@gmail.com>
2026-07-15 10:48:05 -0500

    Adding keys endpoint to authorized requests

--- services/ft_security/src/main/java/net/ft/ft_security/config/SecurityConfig.java
+++ services/ft_security/src/main/java/net/ft/ft_security/config/SecurityConfig.java
@@ -21,7 +21,8 @@ public class SecurityConfig{
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/login",
-                                "/api/v1/auth/register")
+                                "/api/v1/auth/register",
+                                "/api/v1/.well-known/*")
                         .permitAll()
                         .anyRequest().authenticated()
                 );
