4c16c4568fc7ea324430730c8a9441362704f07a
Casey-Lux <casey.lux.000@gmail.com>
2026-07-15 09:25:22 -0500

    Adding endpoint for public key and updating .gitignore for RSA Keys

---
+++ services/ft_security/src/main/java/net/ft/ft_security/controller/KeyController.java
@@ -0,0 +1,26 @@
+package net.ft.ft_security.controller;
+
+import net.ft.ft_security.dto.AuthRequest;
+import net.ft.ft_security.dto.RegisterRequest;
+import net.ft.ft_security.model.User;
+import net.ft.ft_security.service.JwtService;
+import net.ft.ft_security.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/.well-known")
+public class KeyController {
+    private final KeyPair keyPair;
+
+    @GetMapping("/public-key")
+    public ResponseEntity<String> getPublicKey() {
+        String encoded = Base64.getEncoder()
+                .encodeToString(keyPair.getPublic().getEncoded());
+        return ResponseEntity.ok(encoded);
+    }
+}
